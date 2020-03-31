4ba1a1c2e296618e1f541c20213210978825c053
kailichou <35256506+kailichou@users.noreply.github.com>
2020-03-31 18:18:05 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 26 15:28:20 2019
+
+@author: kailic
+"""
+
+from rateCalculator import calculator
+
+print(calculator.toAUD(1000))from rateCalculator import calculator
